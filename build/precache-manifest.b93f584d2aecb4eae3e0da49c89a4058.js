eb6599e3ab3f748503013c3012f5782ccde03021
Jake Krayger <jkrayg@protonmail.com>
2020-06-27 14:04:31 -0400

    trying again

---
+++ build/precache-manifest.b93f584d2aecb4eae3e0da49c89a4058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e9f210c2eeaf0d41633744dfbac73f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "978019826b7cec699625",
+    "url": "/static/css/main.0a2e39f6.chunk.css"
+  },
+  {
+    "revision": "7183098c1410b13d3d50",
+    "url": "/static/js/2.432384f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.432384f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978019826b7cec699625",
+    "url": "/static/js/main.2b7dece1.chunk.js"
+  },
+  {
+    "revision": "0709e6913894a463946d",
+    "url": "/static/js/runtime-main.abb7946e.js"
+  }
+]);
