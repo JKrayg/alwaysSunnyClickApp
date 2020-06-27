15506fb7f48374a514700936b0e2c93a8c1d61ff
Jake Krayger <jkrayg@protonmail.com>
2020-06-27 09:57:39 -0400

    some styling

--- always-sunny/src/App.js
+++ always-sunny/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import gang from "./gang.json"
 import TheGang from "./components/TheGang"
 import Header from "./components/Header"
+import Instructions from "./components/Instructions"
 import Shuffle from "shuffle-array"
 
 class App extends React.Component {
@@ -74,19 +75,21 @@ render() {
           score = { this.state.score }
           streak = { this.state.streak }
           answer = {this.state.answer } 
-            />
-          {this.state.gang.map(member => (
-          <TheGang
-            clickGangMember={this.clickGangMember}
-            id={member.id}
-            key={member.id}
-            name={member.name}
-            image={member.image}
-            clicked={member.clicked}
-          />
+        />
+        {this.state.gang.map(member => (
+        <TheGang
+          clickGangMember={this.clickGangMember}
+          id={member.id}
+          key={member.id}
+          name={member.name}
+          image={member.image}
+          clicked={member.clicked}
+        />
         ))}
+        
       </div>
       
+      <Instructions />
     </React.Fragment>
     
     
